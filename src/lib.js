a568b6720ead4da2475cb30f20dd2c1af597dec5
Veselin Nikolov <124354161+VeskoNik@users.noreply.github.com>
2023-05-05 21:37:55 +0300

    Update lib.js

--- src/lib.js
+++ src/lib.js
@@ -1,6 +1,6 @@
-import {html,render} from "./lib/lit-html.js"
-import { until } from "./lib/directives/until.js"
-import page from "./lib/page.mjs"
+import {html,render} from "../lib/lit-html.js"
+import { until } from "../lib/directives/until.js"
+import page from "../lib/page.mjs"
 
 export {
     html,
